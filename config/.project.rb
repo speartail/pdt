8f0ccc9b577bbef5bd4fa7d27f721c80be6e42a9
Peter Hoeg <p.hoeg@northwind.sg>
2011-07-26 20:28:42 +0800

    updated template files with new tags

--- config/.project.rb
+++ config/.project.rb
@@ -1,3 +1,4 @@
+set :project, 'someproject'
 set :domain, 'somedomain.com'
 
 # you can override all other settings in here
--- config/deploy/.SAMPLE.rb
+++ config/deploy/.SAMPLE.rb
@@ -9,7 +9,8 @@ set :db_host, ''
 set :db_name, ''
 set :db_user, ''
 set :db_pass, ''
-set :db_prefix, 'wp_'
+set :db_prefix, 'wp_'   # WordPress default
+set :db_prefix, 'modx_' # MODx default
 
 set :srcdir, "/home/#{user}/shared/#{host}"
 set :appdir, "/home/#{user}/#{host}"
