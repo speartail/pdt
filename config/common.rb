077cf4c4901808886464f9c49111540d0c373577
Peter Hoeg <peter@speartail.com>
2012-01-20 16:04:37 +0800

    add: email

--- config/common.rb
+++ config/common.rb
@@ -1,3 +1,5 @@
+EMAIL = 'support@speartail.com'
+
 set :mag_locale , 'en_us'
 set :mag_timezone, 'Asia/Singapore'
 set :mag_currency, 'SGD'
@@ -6,8 +8,9 @@ set :mag_secure_url, ''
 set :mag_secure_admin, 'no'
 set :mag_admin_firstname, 'Admin'
 set :mag_admin_lastname, 'User'
-set :mag_admin_username, 'support@northwind.sg'
+set :mag_admin_username, EMAIL
 set :mag_admin_password, 'super_shazza'
+set :mag_admin_email, EMAIL
 
 set :wp_debug, false
 set :wp_lang, ''
