ea773b9cd691f03c356b09561262127bce4dbd4a
Peter Hoeg <peter@speartail.com>
2012-04-01 14:42:28 +0800

    do not get confused by multiple css directories

--- config/compass.rb
+++ config/compass.rb
@@ -10,14 +10,20 @@ if Dir.exists? File.join(@base, 'skin')
 elsif Dir.exists? File.join(@base, 'wp-content')
   # wordpress
   @base = File.join @base, 'wp-content', 'themes', 'default'
-elsif Dir.exists? File.join
+elsif Dir.exists? File.join(@base, 'some_random_modx_path')
+  # modx
+  @base = File.join @base, 'gibblygob'
 else
   puts 'Unable to find project'
 end
 
+found_css = false
 [ 'css', 'CSS', 'stylesheets' ].each do |d|
   dir = File.join @base, d
-  css_dir = dir if Dir.exists? dir
+  if Dir.exists?(dir) && !found_css
+    found_css = true
+    css_dir = dir
+  end
 end
 sass_dir = File.join @base, 'sass'
 images_dir = File.join @base, 'images'
